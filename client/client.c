c5d4e35e915b518a88e59c553ef156972755b222
luckzhangfengbo <1819067326@qq.com>
2020-07-02 10:21:32 +0800

    linux

--- client/client.c
+++ client/client.c
@@ -1,18 +1,16 @@
-/*************************************************************************
-	> File Name: client.c
-	> Author: 
-	> Mail: 
-	> Created Time: 2020年06月04日 星期四 19时50分30秒
- ************************************************************************/
 #include "../common/head.h"
 #include "../common/udp_client.h"
 #include "../common/client_recver.h"
 #include "../common/game.h"
+#include "../common/send_chat.h"
+#include "../common/send_ctl.h"
 
 char server_ip[20] = {0};
 int server_port = 0;
 char *conf = "./football.conf";
 
+struct FootBallMsg chat_msg;
+struct FootBallMsg ctl_msg;
 
 int sockfd;
 
@@ -25,32 +23,40 @@ void logout(int signum) {
 }
 
 int main(int argc, char **argv) {
+        
+    pthread_t draw_t;
+
     int opt;
-    pthread_t recv_t, draw_t;
     struct LogRequest request;
     struct LogResponse response;
+    pthread_t recv_t;
     bzero(&request, sizeof(request));
+    bzero(&chat_msg, sizeof(struct FootBallMsg));
+    bzero(&ctl_msg, sizeof(struct FootBallMsg));
+    
+    chat_msg.type = FT_MSG;
+    ctl_msg.type = FT_CTL;
+
     while ((opt = getopt(argc, argv, "h:p:n:t:m:")) != -1) {
-        switch (opt) {
-            case 'h': {
-                strcpy(server_ip, optarg);
-            } break;
-            case 'p': {
-                server_port = atoi(optarg);
-            } break;
-            case 'n': {
-                strcpy(request.name, optarg);
-            } break;
-            case 't': {
-                request.team = atoi(optarg);
-            } break;
-            case 'm': {
-                strcpy(request.msg, optarg);
-            } break;
-            default: {
-                fprintf(stderr, "Usage : %s [-h host] [-p port]\n", argv[0]);
-                exit(1);
-            }
+        switch(opt) {
+        case 'h':
+            strcpy(server_ip, optarg);
+            break;
+        case 'p':
+            server_port = atoi(optarg);
+            break;
+        case 'n':
+            strcpy(request.name, optarg);
+            break;
+        case 't':
+            request.team = atoi(optarg);
+            break;
+        case 'm':
+            strcpy(request.msg, optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage : %s [-h host] [-p port]!\n", argv[0]);
+            exit(1);
         }
     }
 
@@ -58,87 +64,122 @@ int main(int argc, char **argv) {
     argv += (optind - 1);
 
     if (argc > 1) {
-        fprintf(stderr, "Usage : %s [-h host] [-p port]\n", argv[0]);
+        fprintf(stderr, "Usage : %s [-h host] [-p port]!\n", argv[0]);
         exit(1);
     }
-
+    
     if (!server_port) server_port = atoi(get_value(conf, "SERVERPORT"));
     if (!strlen(server_ip)) strcpy(server_ip, get_value(conf, "SERVERIP"));
     if (!strlen(request.name)) strcpy(request.name, get_value(conf, "NAME"));
-    if (!strlen(request.msg)) strcpy(request.msg, get_value(conf, "LOGMSG"));
+    if (!strlen(response.msg)) strcpy(response.msg, get_value(conf, "LOGMSG"));
     if (!request.team) request.team = atoi(get_value(conf, "TEAM"));
-
+    
     court.width = atoi(get_value(conf, "COLS"));
-    court.height = atoi(get_value(conf, "LINES"));
-    court.start.x = 1;
+    court.heigth = atoi(get_value(conf, "LINES"));
+    court.start.x = 3;
     court.start.y = 1;
-
-
+    
     signal(SIGINT, logout);
+    
+
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(server_port);
     server.sin_addr.s_addr = inet_addr(server_ip);
 
     socklen_t len = sizeof(server);
-
-DBG(GREEN "INFO" NONE
-              " : server_ip = %s, server_port = %d, name = %s, team = %s, "
-              "logmsg = %s\n",
-        server_ip, server_port, request.name, (request.team ? "BLUE" : "RED"),
-        request.msg);
+    
+    DBG(GREEN"INFO"NONE" : server_ip = %s : server_port = %d name = %s, team = %s, logmasg = %s\n", server_ip, server_port, request.name, (request.team ? "BULE" : "RED"), request.msg);
 
     if ((sockfd = socket_udp()) < 0) {
         perror("socket_udp");
         exit(1);
     }
 
-    sendto(sockfd, (void *)&request, sizeof(request), 0,
-           (struct sockaddr *)&server, len);
+    sendto(sockfd, (void *)&request, sizeof(request), 0, (struct sockaddr *)&server, len);
+    
+   fd_set set;
+   FD_ZERO(&set);
+   FD_SET(sockfd, &set);
+   struct timeval tv;
+   tv.tv_sec = 5;
+   tv.tv_usec = 0;
 
-    fd_set set;
-    FD_ZERO(&set);
-    FD_SET(sockfd, &set);
-    struct timeval tv;
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-
-    int retval = select(sockfd + 1, &set, NULL, NULL, &tv);
-    if (retval == -1) {
-        perror("select");
-        exit(1);
-    } else if (retval) {
-        int ret = recvfrom(sockfd, (void *)&response, sizeof(response), 0,
-                           (struct sockaddr *)&server, &len);
+   int retval = select(sockfd + 1, &set, NULL, NULL, &tv);
+   if (retval == -1) {
+       perror("select");
+       exit(1);
+   } else if (retval) {
+        int ret = recvfrom(sockfd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&server, &len);
         if (ret != sizeof(response) || response.type) {
-            DBG(RED
-                "ERROR : " NONE
-                "The Game Server refused your login request!\n\t This may be "
-                "helpful: %s\n",
-                response.msg);
-            exit(1);
-        }
-    } else {
-        DBG(RED "ERROR : " NONE "The Game Server is offline.\n");
+            DBG(RED"ERROR : "NONE"The Game Server refused your login.\n\rThis May be helpfull: %s\n", response.msg); 
         exit(1);
+        }
+   } else {
+       DBG(RED"ERROR : "NONE"The Game Server is out of service.\n");
+       exit(1);
+   }
+
+
+    DBG(GREEN"SERVER : "NONE" %s \n", response.msg);
+    connect(sockfd, (struct sockaddr *)&server, len);
+    //recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&server, &len);
+#ifndef _D    
+    pthread_create(&draw_t, NULL, draw, NULL);//Draw interface 
+#endif
+    pthread_create(&recv_t, NULL, client_recv, NULL);
+    
+
+    signal(14, send_ctl);
+
+    struct itimerval itimer;
+    itimer.it_interval.tv_sec = 0;
+    itimer.it_interval.tv_usec = 100000;
+    itimer.it_value.tv_sec = 0;
+    itimer.it_value.tv_usec = 100000;
+
+    setitimer(ITIMER_REAL, &itimer, NULL);
+    
+    noecho();
+    cbreak();
+    keypad(stdscr, TRUE);
+    while (1) {
+        int c = getchar(); //getch 用UP，DOWN， LEFT， RIGHT
+        switch (c) {
+            case 'a':
+                ctl_msg.ctl.dirx -= 2;
+                break;
+            case 'd':
+                ctl_msg.ctl.dirx += 2;
+                break;
+            case 'w':
+                ctl_msg.ctl.diry -= 1;
+                break;
+            case 's':
+                ctl_msg.ctl.diry += 1;
+                break;
+            case 13: //回车
+                send_chat();
+                break;
+            case ' ':
+                show_strength();
+                break;
+            default:
+                break;
+        }
     }
 
-    DBG(GREEN "SERVER : " NONE " %s \n", response.msg);
-    connect(sockfd, (struct sockaddr *)&server, len);
-#ifndef _D
-    pthread_create(&draw_t, NULL, draw, NULL);
-#endif
-    pthread_create(&recv_t, NULL, client_recv, NULL);  
-    while (1) {
+    /*while (1) {
         struct FootBallMsg msg;
-        memset(&msg, 0, sizeof(msg));
+        memset(msg.msg, 0, sizeof(msg.msg));
         msg.type = FT_MSG;
-        DBG(YELLOW "Input Message : " NONE);
-        w_gotoxy_puts(Write, 1, 1, "Input Message : ");
-        mvwscanw(Write, 2, 1, "%[^\n]s", msg.msg);
-        if(strlen(msg.msg))
-            send(sockfd, (void *)&msg, sizeof(msg), 0);
-    }
+        DBG(YELLOW"Input Message :"NONE);
+        w_gotoxy_puts(Write, 1, 0, "Input Message : ");
+        mvwscanw(Write, 1, 1, "%[^\n]s", msg.msg);
+        if (strlen(msg.msg) == 0) continue;
+        send(sockfd, (void *)&msg, sizeof(msg), 0);
+        werase(Write);
+    }*/
 
     sleep(10);
 
--- server/server.c
+++ server/server.c
@@ -1,125 +1,146 @@
-/*************************************************************************
-	> File Name: server.c
-	> Author: 
-	> Mail: 
-	> Created Time: 2020年06月02日 星期二 18时14分47秒
- ************************************************************************/
-
-#include"../common/udp_server.h"
-#include"../common/head.h"
-#include "../common/heart_beat.h"
-#include "../common/sub_reactor.h"
-#include"../common/udp_epoll.h"
+#include "../common/udp_epoll.h"
+#include "../common/head.h"
+#include "../common/udp_server.h"
 #include "../common/thread_pool.h"
-#include"../common/game.h"
-#include"../common/server_exit.h"
+#include "../common/sub_reactor.h"
+#include "../common/heart_beat.h"
+#include "../common/game.h"
+#include "../common/server_exit.h"
+#include "../common/server_re_draw.h"
+char *conf = "./server.conf"; //弄成全局的
 
-
-char *conf = "./server.conf";
-struct User *rteam; //存放两个队的用户端
-struct User *bteam;
-int data_port;
-int port = 0;
 //struct Map court;
+
+struct User *rteam;
+struct User *bteam;
 
 int repollfd, bepollfd;
 
-int main(int argc, char **argv){
+//int epoll_fd;
+int port = 0;
+
+int main(int argc, char **argv) {
     int opt, listener, epoll_fd;
+    //int port = 0;
     pthread_t draw_t, red_t, blue_t, heart_t;
-    while((opt = getopt(argc, argv, "p;")) != -1){
-        switch(opt){
-            case 'p':
-                port = atoi(optarg);
-                break;
-            default :
-                fprintf(stderr , "Usage: %s [-p port]\n", argv[0]);
-                exit(1);
+    while ((opt = getopt(argc, argv, "p:")) != -1) {//:表示后面跟着有参数
+        switch (opt) {
+        case 'p':
+            port = atoi(optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-p port]\n", argv[0]);
+            exit(1);
         }
     }
-    argc -= (optind - 1);
-    argv += (optind - 1);
 
-    if(argc > 1){
-        fprintf(stderr , "Usage: %s [-p port]\n", argv[0]);
+    //在getopt的执行中，每次进入都会更新optind变量，该变量指向下一个argv参数
+    //
+    argc -= (optind - 1); //个数减少
+    argv += (optind  -1); //指针后移
+    
+    if (argc > 1) {
+        fprintf(stderr, "Usage: %s [-p port]\n", argv[0]);
         exit(1);
     }
     
-
-    if(!port) port = atoi(get_value(conf, "PORT"));
-    data_port = atoi(get_value(conf, "DATAPORT"));
+    if (!port) port = atoi(get_value(conf, "PORT")); //未指定端口
     court.width = atoi(get_value(conf, "COLS"));
-    court.height = atoi(get_value(conf, "LINES"));
-    court.start.x = 1;
+    court.heigth = atoi(get_value(conf, "LINES"));
+    court.start.x = 3;
     court.start.y = 1;
 
     rteam = (struct User *)calloc(MAX, sizeof(struct User));
     bteam = (struct User *)calloc(MAX, sizeof(struct User));
     
-    if((listener = socket_create_udp(port)) < 0){
+    if ((listener = socket_create_udp(port)) < 0) {
         perror("socket_create_udp");
         exit(1);
     }
-    DBG(GREEN"INFO"NONE" : Server start on Prt %d\n",port);
-#ifndef _D
-    pthread_create(&draw_t, NULL, draw, NULL);
+
+    DBG(GREEN"INFO"NONE" : Server start on Port %d\n", port);//DEBUG，-D _D
+#ifndef _D    
+    pthread_create(&draw_t, NULL, draw, NULL);//Draw interface
 #endif
-    epoll_fd = epoll_create(MAX * 2);
+    epoll_fd = epoll_create(MAX * 2);//由epoll_create 生成的epoll专用的文件描述符；
+    //该函数生成一个epoll专用的文件描述符，其中的参数是指定生成描述符的最大范围。在linux-2.4.32内核中根据size大小初始化哈希表的大小，在linux2.6.10内核中该参数无用，使用红黑树管理所有的文件描述符，而不是hash。
     repollfd = epoll_create(MAX);
     bepollfd = epoll_create(MAX);
 
-    if(epoll_fd < 0 || repollfd < 0 || bepollfd < 0){
-           perror("epoll_create");
-           exit(1);
+
+    if (epoll_fd < 0 || repollfd < 0 || bepollfd < 0) {
+        perror("epoll_creat");
+        exit(1);
     }
+
     struct task_queue redQueue;
     struct task_queue blueQueue;
-    //任务队列初始化
+
     task_queue_init(&redQueue, MAX, repollfd);
     task_queue_init(&blueQueue, MAX, bepollfd);
-    //创建线程做函数sub_reactor
+
     pthread_create(&red_t, NULL, sub_reactor, (void *)&redQueue);
     pthread_create(&blue_t, NULL, sub_reactor, (void *)&blueQueue);
     pthread_create(&heart_t, NULL, heart_beat, NULL);
-    signal(SIGINT, server_exit); 
-    //主反应堆
-    struct epoll_event ev, events[MAX * 2];
 
-    ev.events = EPOLLIN;
+    signal(SIGINT, server_exit);
+
+    //声明epoll_event结构体的变量,ev用于注册事件,数组用于回传要处理的事件
+    struct epoll_event ev, events[MAX * 2]; //用于回传代处理事件的数组
+    
+    //设置要处理的事件类型
+    ev.events = EPOLLIN; //注册关注的事件为是否可读
+    
+    //设置与要处理事件相关的文件描述符
     ev.data.fd = listener;
-   
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listener, &ev);
+
+    //注册epoll事件
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listener, &ev);//将要关注的事情加入到epoll_fd中（该函数用于控制某个文件描述符上的事件，可以注册事件，修改事件，删除事件。）
+    
     struct sockaddr_in client;
     socklen_t len = sizeof(client);
     
-    Show_Message( , ,"Waiting for Login.", 1);
+    signal(14, re_draw);
 
-    while(1){
-        DBG(YELLOW"Main Thread"NONE" : Before epoll_wait\n");
-        int nfds = epoll_wait(epoll_fd, events, MAX * 2, -1);
+    struct itimerval itimer;
+    itimer.it_interval.tv_sec = 0;
+    itimer.it_interval.tv_usec = 50000;
+    itimer.it_value.tv_sec = 5;
+    itimer.it_value.tv_usec = 0;
+    
+    setitimer(ITIMER_REAL, &itimer, NULL);
+
+    Show_Message( , , "Waiting for Login.", 1 ); //sys 1
+    while (1) {
+        //w_gotoxy_puts(Message, 1, 1, "waiting for login");
+        DBG(YELLOW"Main Thread"NONE" : before epoll_wait\n");
+        int nfds = epoll_wait(epoll_fd, events, MAX * 2, -1); //该函数用于轮询I/O事件的发生；
         DBG(YELLOW"Main Thread"NONE" : After epoll_wait\n");
-
-        for(int i = 0; i < nfds; i++){
-            char buff[512] = {0};
+        
+        for (int i = 0; i < nfds; i++) {
             struct User user;
+            char buff[512] = {0};
             DBG(YELLOW"EPOLL"NONE" : Doing with %dth fd\n", i);
-            if(events[i].data.fd == listener){
-               int new_fd =  udp_accept(epoll_fd, listener, &user);
-                
+            if (events[i].data.fd == listener) {
+                //accept();
+                int new_fd = udp_accept(epoll_fd, listener, &user);
                 if (new_fd > 0) {
-                    sprintf(buff,"%s Login the Game.", user.name);
-                    DBG(YELLOW "Main Thread" NONE
-                               " : Add %s to %s sub_reactor.\n",
-                        user.name, (user.team ? "BLUE" : "RED"));
-                    add_to_sub_reactor(&user); //用于将用户加入到反应堆中
-                    Show_Message(, ,buff, 1);
+                    sprintf(buff, "%s login the Game.", user.name);
+                    DBG(YELLOW"Main Thread"NONE" :Add %s to %s sub_reactor.\n", user.name, (user.team ? "BLUE" : "RED"));
+                    add_to_sub_reactor(&user);
+                    Show_Message(, , buff, 1);
                 }
-            }else {
+            } else {
                 recv(events[i].data.fd, buff, sizeof(buff), 0);
-                printf(PINK "RECV" NONE " : %s\n", buff);
+                printf(PINK"RECV"NONE" : %s\n", buff);
             }
+            //char info[1024] = {0};
+            //recvfrom(events[i].data.fd, (void *)info, sizeof(info), 0, (struct sockaddr *)&client, &len);
+            
+            //sprintf(info, "Login : %s : %d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+            //w_gotoxy_puts(Message, 1, 2, info);
         }
-    }
 
+    }
     return 0;
 }
