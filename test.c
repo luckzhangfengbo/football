a590d9c44dd75dfca0d4e8bde5e96f117cdaae53
luckzhangfengbo <1819067326@qq.com>
2020-07-07 18:58:48 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 07 Jul 2020 06:57:55 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main() {
+
+    printf("ans");
+    return 0;
+}
